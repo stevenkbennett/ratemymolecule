0c5a252da63733a2c216b8ce4c97e4b27d7d7471
Steven Bennett <stevenbennettonline@gmail.com>
2019-04-25 15:41:46 +0100

    Add config

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CRSF_ENABLED = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    #: For local or non-local databases.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'ratemymolecule@gmail.com'
+    MAIL_PASSWORD = 'ratemymolecule123'
+    ADMINS = ['ratemymolecule@gmail.com']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
