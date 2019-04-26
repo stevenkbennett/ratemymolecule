0291e08e3464ced8755ead7f98d63e166b8238e9
Steven Bennett <stevenbennettonline@gmail.com>
2019-04-26 11:28:29 +0100

    Updated

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CRSF_ENABLED = True
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
     #: For local or non-local databases.
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
